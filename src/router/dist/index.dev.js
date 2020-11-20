658409ace38878d9323acca5234f50b9aa1808d7
mrPlotnik <plotnik1992@gmail.com>
2020-11-20 19:27:56 +0700

    fix

--- src/router/dist/index.dev.js
+++ src/router/dist/index.dev.js
@@ -1,40 +1,40 @@
-"use strict";
+'use strict'
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
-});
-exports["default"] = void 0;
+})
+exports.default = void 0
 
-var _vue = _interopRequireDefault(require("vue"));
+var _vue = _interopRequireDefault(require('vue'))
 
-var _vueRouter = _interopRequireDefault(require("vue-router"));
+var _vueRouter = _interopRequireDefault(require('vue-router'))
 
-var _Home = _interopRequireDefault(require("../views/Home.vue"));
+var _Home = _interopRequireDefault(require('../views/Home.vue'))
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof (obj) { if (typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol') { _typeof = function _typeof (obj) { return typeof obj } } else { _typeof = function _typeof (obj) { return obj && typeof Symbol === 'function' && obj.constructor === Symbol && obj !== Symbol.prototype ? 'symbol' : typeof obj } } return _typeof(obj) }
 
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+function _getRequireWildcardCache () { if (typeof WeakMap !== 'function') return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache () { return cache }; return cache }
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard (obj) { if (obj && obj.__esModule) { return obj } if (obj === null || _typeof(obj) !== 'object' && typeof obj !== 'function') { return { default: obj } } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj) } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc) } else { newObj[key] = obj[key] } } } newObj.default = obj; if (cache) { cache.set(obj, newObj) } return newObj }
 
-_vue["default"].use(_vueRouter["default"]);
+_vue.default.use(_vueRouter.default)
 
 var routes = [{
   path: '/',
   name: 'Home',
-  component: _Home["default"]
+  component: _Home.default
 }, {
   path: '/login',
   name: 'login',
   meta: {
     layout: 'empty'
   },
-  component: function component() {
+  component: function component () {
     return Promise.resolve().then(function () {
-      return _interopRequireWildcard(require('../views/Login.vue'));
-    });
+      return _interopRequireWildcard(require('../views/Login.vue'))
+    })
   }
 }, {
   path: '/categories',
@@ -42,16 +42,16 @@ var routes = [{
   meta: {
     layout: 'main'
   },
-  component: function component() {
+  component: function component () {
     return Promise.resolve().then(function () {
-      return _interopRequireWildcard(require('../views/Categories.vue'));
-    });
+      return _interopRequireWildcard(require('../views/Categories.vue'))
+    })
   }
-}];
-var router = new _vueRouter["default"]({
+}]
+var router = new _vueRouter.default({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: routes
-});
-var _default = router;
-exports["default"] = _default;
+})
+var _default = router
+exports.default = _default
