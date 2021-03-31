5fa03cd9cdf14c7758636cf6bb333df6fe8f32be
mrPlotnik <plotnik1992@gmail.com>
2021-03-31 17:59:27 +0700

    Продолжаю проект. 14 урок завершен

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from 'firebase/app'
 
 // Регистрация Router как плагина
 Vue.use(VueRouter)
@@ -10,7 +11,7 @@ const routes = [
     // Главная страница
     path: '/',
     name: 'home',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     // ИзиЛоадинг для ускорения загрузки
     component: () => import('../views/Home.vue')
   },
@@ -29,31 +30,31 @@ const routes = [
   {
     path: '/categories',
     name: 'categories',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/Categories.vue')
   },
   {
     path: '/history',
     name: 'history',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/History.vue')
   },
   {
     path: '/planning',
     name: 'planning',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/Planning.vue')
   },
   {
     path: '/record',
     name: 'record',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/Record.vue')
   },
   {
     path: '/profile',
     name: 'profile',
-    meta: { layout: 'main' },
+    meta: { layout: 'main', auth: true },
     component: () => import('../views/Profile.vue')
   },
   {
@@ -71,6 +72,27 @@ const router = new VueRouter({
   // Базовый URL
   base: process.env.BASE_URL,
   routes
+})
+
+// Данный метод будет вызываться перед каждой сменой роута
+// Будем проверять на наличие авторизации
+// to - тот роут, куда мы идем
+// from - тот роут, откуда мы пришли
+// next - функция, которую нужно выполнить, если мы хотим продолжить переход
+router.beforeEach((to, from, next) => {
+  // Сначала необходимо понять, если у пользователя авторицация
+  const currentUser = firebase.auth().currentUser
+  // Далее проверяем требует ли текущий роут авторизации
+  const requiredAuth = to.matched.some(record => record.meta.auth)
+
+  // Проверяем, если текущий роут требует авторизации и нет авторизованного пользователя, то...
+  if (requiredAuth && !currentUser) {
+    // Направляем на страницу логина
+    next('/login?message=login')
+  } else {
+    // Иначе, продолжаем выполнение
+    next()
+  }
 })
 
 export default router
